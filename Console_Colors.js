e13d739cab72de83a8497af6925d369044dcb96c
Neeraj Kumar <nk7075034@gmail.com>
2023-06-21 12:23:29 +0530

    Learned how to print text with custom styling in the console of browsers

---
+++ Console_Colors.js
@@ -0,0 +1,8 @@
+// Printing colors in console
+console.log("%c%s", "background: yellow; color: red; font-size: 24px; padding: 0.5rem 1rem; border-radius: 0.5rem;", "Warning");
+
+/**
+ * The %c tells the first argument is the CSS
+ * The %s stands for the string that is needed to be styled
+ * We can write multiple strings with their corresponding CSS
+ */
